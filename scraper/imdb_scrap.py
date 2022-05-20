0f2ffb89ec9bb4448b07375755da36063ffa7bef
Yash Jain <yash0307jain@gmail.com>
2022-05-21 01:18:34 +0530

    getting all the reviews and exporting it into json file

--- scraper/imdb_scrap.py
+++ scraper/imdb_scrap.py
@@ -1,26 +1,51 @@
-from pathlib import Path
-import selenium
 from selenium import webdriver
-import requests
-from bs4 import BeautifulSoup
+import json
 import time
 
 try:
+    MOVIE_NAME = "Panchayat"
     CHROME_DRIVER_BINARY = "scraper/chromedriver"
     options = webdriver.ChromeOptions()
     options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
     driver = webdriver.Chrome(CHROME_DRIVER_BINARY, chrome_options=options)
 
+    # Go to the URL
+    driver.maximize_window()
     url = "https://www.imdb.com/"
     driver.get(url)
 
-    search_area = driver.find_element_by_id("suggestion-search").send_keys("Doctor Strange")
+    # Write on the search text field
+    search_area = driver.find_element_by_id("suggestion-search").send_keys(MOVIE_NAME)
 
-    search_button = driver.find_element_by_id("suggestion-search-button").click()
+    # Click on the search button
+    driver.find_element_by_id("suggestion-search-button").click()
 
-    movie_table = driver.find_element_by_xpath("//table[@class='findList']/tbody/tr/td[@class='result_text']/a").click()
-    print(movie_table.get_attribute('innerHTML'))
+    # Go to the first movie in the list
+    driver.find_element_by_xpath("//table[@class='findList']/tbody/tr/td[@class='result_text']/a").click()
 
-    time.sleep(2)
+    # Click on the review button
+    driver.find_element_by_xpath("//div[@data-testid='reviews-header']/a").click()
+
+    # Get all the reviews
+    reviews_obj = []
+    while True:
+        load_more = driver.find_element_by_class_name("ipl-load-more__button")
+        if load_more.text == 'Load More':
+            load_more.click()
+            time.sleep(3)
+        else:
+            reviews_obj = driver.find_elements_by_xpath("//div[@class='text show-more__control']")
+            break
+    
+    # Quit the driver
+    driver.quit()
+
+    reviews = []
+    for review in reviews_obj:
+        reviews.append(review.text)
+    
+    data = {'reviews': reviews}
+    with open(f'data/{MOVIE_NAME}.json', 'w') as jsonfile:
+        json.dump(data, jsonfile)
 except Exception as e:
     print(str(e))
