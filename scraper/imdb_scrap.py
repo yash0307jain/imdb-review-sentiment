b627699c8c5a29ce96c65146f7ef96e282097061
Yash Jain <yash0307jain@gmail.com>
2022-05-21 09:09:30 +0530

    exporting of scrap reviews has been done

--- scraper/imdb_scrap.py
+++ scraper/imdb_scrap.py
@@ -1,51 +1,60 @@
 from selenium import webdriver
 import json
 import time
+import os.path
 
-try:
-    MOVIE_NAME = "Panchayat"
-    CHROME_DRIVER_BINARY = "scraper/chromedriver"
-    options = webdriver.ChromeOptions()
-    options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
-    driver = webdriver.Chrome(CHROME_DRIVER_BINARY, chrome_options=options)
+def scrap_reviews(movie_name: str) -> None:
+    try:
+        if os.path.exists(f"data/{movie_name}.json"):
+            print("Already exists")
+            return
 
-    # Go to the URL
-    driver.maximize_window()
-    url = "https://www.imdb.com/"
-    driver.get(url)
+        MOVIE_NAME = movie_name
+        CHROME_DRIVER_BINARY = "scraper/chromedriver"
+        options = webdriver.ChromeOptions()
+        options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+        driver = webdriver.Chrome(CHROME_DRIVER_BINARY, chrome_options=options)
 
-    # Write on the search text field
-    search_area = driver.find_element_by_id("suggestion-search").send_keys(MOVIE_NAME)
+        # Go to the URL
+        driver.maximize_window()
+        url = "https://www.imdb.com/"
+        driver.get(url)
 
-    # Click on the search button
-    driver.find_element_by_id("suggestion-search-button").click()
+        # Write on the search text field
+        search_area = driver.find_element_by_id("suggestion-search").send_keys(MOVIE_NAME)
+        time.sleep(2)
 
-    # Go to the first movie in the list
-    driver.find_element_by_xpath("//table[@class='findList']/tbody/tr/td[@class='result_text']/a").click()
+        # Click on the search button
+        driver.find_element_by_id("suggestion-search-button").click()
 
-    # Click on the review button
-    driver.find_element_by_xpath("//div[@data-testid='reviews-header']/a").click()
+        # Go to the first movie in the list
+        driver.find_element_by_xpath("//table[@class='findList']/tbody/tr/td[@class='result_text']/a").click()
 
-    # Get all the reviews
-    reviews_obj = []
-    while True:
-        load_more = driver.find_element_by_class_name("ipl-load-more__button")
-        if load_more.text == 'Load More':
-            load_more.click()
-            time.sleep(3)
-        else:
-            reviews_obj = driver.find_elements_by_xpath("//div[@class='text show-more__control']")
-            break
-    
-    # Quit the driver
-    driver.quit()
+        # Click on the review button
+        driver.find_element_by_xpath("//div[@data-testid='reviews-header']/a").click()
 
-    reviews = []
-    for review in reviews_obj:
-        reviews.append(review.text)
-    
-    data = {'reviews': reviews}
-    with open(f'data/{MOVIE_NAME}.json', 'w') as jsonfile:
-        json.dump(data, jsonfile)
-except Exception as e:
-    print(str(e))
+        # Get all the reviews
+        reviews_obj = []
+        while True:
+            load_more = driver.find_element_by_class_name("ipl-load-more__button")
+            if load_more.text == 'Load More':
+                load_more.click()
+                time.sleep(3)
+            else:
+                reviews_obj = driver.find_elements_by_xpath("//div[@class='text show-more__control']")
+                break
+        
+        reviews = []
+        for review in reviews_obj:
+            if review.text != "":
+                reviews.append(review.text)
+        
+        # Export reviews
+        data = {'reviews': reviews}
+        with open(f'data/{MOVIE_NAME}.json', 'w') as jsonfile:
+            json.dump(data, jsonfile)
+
+        # Quit the driver
+        driver.quit()
+    except Exception as e:
+        print(str(e))
